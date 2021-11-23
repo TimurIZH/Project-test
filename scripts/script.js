59af7edb99e32b6bba3d4c25662d38d9f9fd696b
Timur <6maly6@mail.ru>
2021-11-23 18:30:26 +0400

    first

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+//alert("Hi");
+//const result = confirm("Ты тут?");
+//console.log(result);
+
+//const answer = prompt("вам есть 18?","18");
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя', '');
+//answers[1] = prompt('Как ваша фамилия', '');
+//answers[2] = prompt('Сколько вам лет', '');
+
+//document.write(answers);
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = 'Timur';
+
+//alert(`Привет, ${user}`);
+
+//console.log('arr' + "-object");
+//console.log(4 + "-object");
+
+let incr = 10,
+    decr = 10;
+ 
+//incr++;
+//decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);    
+console.log(isChecked || !isClose); 
+
+
+
